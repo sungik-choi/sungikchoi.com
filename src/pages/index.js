71484e96816a044b34e6212003910350feb4145e
최성익 <tjddlr92@gmail.com>
2020-08-07 21:16:27 +0900

    Feat : Index Page Styling
    - Post Grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
+import styled from 'styled-components';
 import Layout from 'components/layout/layout';
 
 const Home = ({ data }) => {
   return (
     <Layout>
-      <p>Hello World</p>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <Link to={node.fields.slug}>
-            <h3>
-              {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
-            </h3>
-            <p>{node.excerpt}</p>
-          </Link>
-        </div>
-      ))}
+      <PostGrid>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link to={node.fields.slug}>
+              <Thumbnail src="https://miro.medium.com/max/2880/1*GgLVXXfYDuS08V1SQQQaeQ.jpeg" />
+              <h3>{node.frontmatter.tag}</h3>
+              <h3>
+                {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+              </h3>
+              <p>{node.frontmatter.desc}</p>
+            </Link>
+          </div>
+        ))}
+      </PostGrid>
     </Layout>
   );
 };
+
+const PostGrid = styled.section`
+  padding-top: 40px;
+  margin: 0 auto;
+  width: 692px;
+`;
+
+const Thumbnail = styled.img`
+  border-radius: 16px;
+`;
 
 export const query = graphql`
   query {
@@ -29,7 +43,9 @@ export const query = graphql`
           id
           frontmatter {
             title
+            tag
             date(formatString: "DD MMMM, YYYY")
+            desc
           }
           fields {
             slug
