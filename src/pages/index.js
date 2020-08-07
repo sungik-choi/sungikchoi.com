928cbd35f92c0212abd6c7811ed1416e675ee187
최성익 <tjddlr92@gmail.com>
2020-08-07 21:16:54 +0900

    Fix : section -> main tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,28 +7,33 @@ const Home = ({ data }) => {
   return (
     <Layout>
       <PostGrid>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <Link to={node.fields.slug}>
-              <Thumbnail src="https://miro.medium.com/max/2880/1*GgLVXXfYDuS08V1SQQQaeQ.jpeg" />
-              <h3>{node.frontmatter.tag}</h3>
-              <h3>
-                {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
-              </h3>
-              <p>{node.frontmatter.desc}</p>
-            </Link>
-          </div>
-        ))}
+        <Post>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <Link to={node.fields.slug}>
+                <Thumbnail src="https://miro.medium.com/max/2880/1*GgLVXXfYDuS08V1SQQQaeQ.jpeg" />
+                <h3>{node.frontmatter.tag}</h3>
+                <h3>
+                  {node.frontmatter.title}{' '}
+                  <span>— {node.frontmatter.date}</span>
+                </h3>
+                <p>{node.frontmatter.desc}</p>
+              </Link>
+            </div>
+          ))}
+        </Post>
       </PostGrid>
     </Layout>
   );
 };
 
-const PostGrid = styled.section`
+const PostGrid = styled.main`
   padding-top: 40px;
   margin: 0 auto;
   width: 692px;
 `;
+
+const Post = styled.article``;
 
 const Thumbnail = styled.img`
   border-radius: 16px;
