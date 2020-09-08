1508c9e9d811a3894546953c192e61d49bea3e72
최성익 <tjddlr92@gmail.com>
2020-09-08 15:41:28 +0900

    Fix : 빌드 오류 수정
    - config 파일에 알맞지 않은 키값. comment는 넣을 수 없음

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,39 +1,37 @@
 import React, { createRef, useLayoutEffect } from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+// import { useStaticQuery, graphql } from 'gatsby';
 
 const src = 'https://utteranc.es/client.js';
 
 const Comment = () => {
   const containerRef = createRef();
 
-  const data = useStaticQuery(
-    graphql`
-      query {
-        site(comment: {}) {
-          id
-          comment {
-            enable
-            repo
-            theme
-          }
-        }
-      }
-    `
-  );
+  // const data = useStaticQuery(
+  //   graphql`
+  //     query {
+  //       site(comment: {}) {
+  //         id
+  //         comment {
+  //           enable
+  //           repo
+  //           theme
+  //         }
+  //       }
+  //     }
+  //   `
+  // );
 
-  const { enable, repo, theme } = data.site.comment;
+  // const { enable, repo, theme } = data.site.comment;
 
   useLayoutEffect(() => {
-    if (!enable) return;
-
     const comment = document.createElement('script');
 
     const attributes = {
       src,
-      repo,
+      repo: 'sungik-choi/blog-comment',
       'issue-term': 'pathname',
       label: 'comment',
-      theme,
+      theme: 'preferred-color-scheme',
       crossOrigin: 'anonymous',
       async: 'true',
     };
