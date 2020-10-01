46536a1188f142288a80450183779d2e9e960a0e
최성익 <tjddlr92@gmail.com>
2020-10-01 18:46:03 +0900

    Feat : utternances에 다이나믹 테마 적용
    - useContext를 통해 theme 변수를 사용한다
    - siteMetaData에 theme가 필요없어졌으므로 함께 삭제 (useTheme에서 선호하는 테마를 확인하고 자동으로 변경해서 적용함)
    - useLayoutEffect 첫번째 줄 : 현재 로컬스토리지에 저장돼있는 테마와 비교해서 같지 않으면 리턴한다.
    - 이유 : setState가 비동기적으로 실행되어 theme가 원치 않는 테마인 채로 밸류값으로 들어가 utternaces가 생성됨. 의존성 배열에 theme가 들어가있으므로, theme가 제대로 변경됐을 때(변경됐다면 로컬스토리지에 그 값이 저장되도록 설정했다) utterances 생성 속성에도 동일한 테마가 들어가도록 설정
    - utternaces 엘리먼트를 문서에서 찾고, 없다면 생성하고, 있다면 postMessage 메서드를 통해 iframe 내부로 theme 변경 메세지를 전달한다. theme 를 변경하는 건 layout 컴포넌트의 테마 토글러 버튼밖에 없으므로, 테마변경 버튼이 클릭되고 theme가 변경되면 테마 변경 메세지도 같이 전달된다.
    - iframe이라그런지 동기적으로 자연스럽게 테마 변경이 되진 않지만 이정도도 만족스러움

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,32 +1,53 @@
-import React, { createRef, useLayoutEffect } from 'react';
+import React, { useRef, useContext, useEffect, useLayoutEffect } from 'react';
+import { ThemeContext } from 'components/layout/layout';
 import { useSiteMetadata } from 'hooks/useSiteMetadata';
 
-const src = 'https://utteranc.es/client.js';
+const src = 'https://utteranc.es';
+const utterancesSelector = 'iframe.utterances-frame';
+const LIGHT_THEME = 'github-light';
+const DARK_THEME = 'github-dark';
 
 const Comment = () => {
+  const theme = useContext(ThemeContext);
+  const themeMode = theme === 'light' ? LIGHT_THEME : DARK_THEME;
+
   const site = useSiteMetadata();
-  const { repo, theme } = site.siteMetadata.utterances;
-  const containerRef = createRef();
+  const { repo } = site.siteMetadata.utterances;
+  const containerRef = useRef(null);
 
-  useLayoutEffect(() => {
+  const postThemeMessage = (elem) => {
+    const utterances = elem.contentWindow;
+    const message = {
+      type: 'set-theme',
+      theme: themeMode,
+    };
+    utterances.postMessage(message, src);
+  };
+
+  const createUtterancesEl = () => {
     const comment = document.createElement('script');
-
     const attributes = {
-      src,
+      src: `${src}/client.js`,
       repo,
-      'issue-term': 'pathname',
+      'issue-term': 'title',
       label: 'comment',
-      theme,
+      theme: themeMode,
       crossOrigin: 'anonymous',
       async: 'true',
     };
-
     Object.entries(attributes).forEach(([key, value]) => {
       comment.setAttribute(key, value);
     });
-
     containerRef.current.appendChild(comment);
-  }, []);
+  };
+
+  useLayoutEffect(() => {
+    const bSameSavedTheme = theme === localStorage.getItem('theme');
+    if (!bSameSavedTheme) return;
+    const utterancesEl = document.querySelector(utterancesSelector);
+    if (utterancesEl) postThemeMessage(utterancesEl);
+    else createUtterancesEl();
+  }, [theme]);
 
   return <div ref={containerRef} />;
 };
