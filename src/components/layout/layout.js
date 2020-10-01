d8e2a40c2f0146195757770931f1f2b4993f7311
최성익 <tjddlr92@gmail.com>
2020-10-01 18:30:56 +0900

    Feat : theme 상태 context api로 child에게 전달
    - utternances 테마를 변경하는 과정에서, 동일한 theme 상태를 유지해야 하는 이슈가 발생했다.
    - useTheme() 커스텀 훅을 통해 가져온 theme 변수를 ThemeContext.Provider에 담아서 comment.js 파일에서도 동일한 theme에 접근할 수 있도록 했음
    - layout 컴포넌트를 사용하는 예제들 중 children에게 props로 넘겨주는 방법들을 알아봤으나 context 쪽이 가장 쉬운 거 같아 이 방법을 사용

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 
 import useTheme from 'hooks/useTheme';
@@ -9,21 +9,23 @@ import GlobalStyle from 'styles/globalStyle';
 
 import NavBar from './navBar';
 
+export const ThemeContext = createContext();
+
 const Layout = ({ children }) => {
   const [theme, themeToggler] = useTheme();
-  // ? 왜 2번 불리면서 theme가 제대로 저장이 안되는거지?
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
-  console.log(theme, themeMode);
+
   const site = useSiteMetadata();
   const { title, description, author } = site.siteMetadata;
   const copyrightStr = `${description}. Copyright © ${author}.`;
 
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={themeMode}>
       <GlobalStyle />
       <Container>
         <NavBar title={title} />
-        {children}
+        <ThemeToggleButton onClick={themeToggler}>테마변경</ThemeToggleButton>
+        <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
         <Footer role="contentinfo">
           <Copyright aria-label="Copyright">{copyrightStr}</Copyright>
         </Footer>
@@ -32,10 +34,18 @@ const Layout = ({ children }) => {
   );
 };
 
+const ThemeToggleButton = styled.button`
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+`;
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
   ${({ theme }) => `min-height: calc(100vh - ${theme.footerHeight})`};
+  background-color: ${({ theme }) => theme.color.white};
 `;
 
 const Footer = styled.footer`
