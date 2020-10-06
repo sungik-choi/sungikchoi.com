be13e349dce1885063cc5991619196d389d7e8b4
최성익 <tjddlr92@gmail.com>
2020-10-06 22:38:46 +0900

    Feat : 토글 버튼 반응형 디자인 추가
    - 차후 플로팅 액션 버튼으로 하는 것보다, 모바일 변경시 우측 상단에 버거메뉴가 생기며 네비게이션 하단에 스위치 버튼으로 있는 편이 더 좋을 거 같음
    - 플로팅 액션 버튼으로 모바일에서 처리하기엔 너무 중요한 위치를 차지하고 있어서 좋지 않은듯

--- src/components/layout/themeToggleButton.js
+++ src/components/layout/themeToggleButton.js
@@ -13,32 +13,36 @@ const ThemeToggleButton = ({ themeToggler }) => {
     <Button onClick={themeToggler}>
       <ButtonBackground />
       <Content>
-        <Icon
-          version="1.1"
-          width="18"
-          height="18"
-          x="0px"
-          y="0px"
-          viewBox="0 0 24 24"
-        >
+        <Icon version="1.1" x="0px" y="0px" viewBox="0 0 24 24">
           <ThemeIcon theme={theme} />
         </Icon>
-        <Text>{LABEL_TEXT}</Text>
+        <Text className="visually-hidden">{LABEL_TEXT}</Text>
       </Content>
     </Button>
   );
 };
 
 const Icon = styled.svg`
+  width: 1.5rem;
+  height: 1.5rem;
   fill: ${({ theme }) => theme.color.icon};
-  margin-bottom: 2px;
+  transform: translate(2px, -1px);
+  @media (min-width: ${({ theme }) => theme.device.lg}) {
+    width: 1.125rem;
+    height: 1.125rem;
+    transform: translate(0, -1px);
+  }
 `;
 
 const ButtonBackground = styled(Background)`
-  border-radius: ${({ theme }) => theme.borderRadius.lg};
+  border-radius: 50%;
   border: none;
   background-color: ${({ theme }) => theme.color.floatingButton};
   box-shadow: 0 3px 15px ${({ theme }) => theme.color.floatingButtonShadow};
+
+  @media (min-width: ${({ theme }) => theme.device.lg}) {
+    border-radius: ${({ theme }) => theme.borderRadius.lg};
+  }
 `;
 
 const Content = styled.div`
@@ -52,6 +56,14 @@ const Content = styled.div`
 const Text = styled.span`
   color: ${({ theme }) => theme.color.floatingButtonText};
   margin-left: 0.375rem;
+
+  @media (min-width: ${({ theme }) => theme.device.lg}) {
+    position: static;
+    width: auto;
+    height: auto;
+    clip: auto;
+    white-space: auto;
+  }
 `;
 
 const Button = styled.button`
@@ -68,7 +80,11 @@ const Button = styled.button`
   padding: ${({ theme }) => theme.sizing.base};
   padding-right: 1.25rem;
   border: 1px solid ${({ theme }) => theme.color.floatingButtonBorder};
-  border-radius: ${({ theme }) => theme.borderRadius.lg};
+  border-radius: 50%;
+
+  @media (min-width: ${({ theme }) => theme.device.lg}) {
+    border-radius: ${({ theme }) => theme.borderRadius.lg};
+  }
 
   &:hover {
     outline: none;
