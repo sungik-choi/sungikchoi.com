0ad978aaea406e7e7e5f5fde3fab25049ddfaf99
최성익 <tjddlr92@gmail.com>
2020-10-13 20:27:54 +0900

    Feat : about 페이지 Layout 컴포넌트 추가

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react';
+import Layout from 'components/layout/layout';
 
 const About = () => {
-  return <div>소개</div>;
+  return (
+    <Layout>
+      <div>소개</div>
+    </Layout>
+  );
 };
 
 export default About;
