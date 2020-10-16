d7250c4443b14365344a179a5251cbe2705c36e5
최성익 <sungik.dev@gmail.com>
2020-10-16 18:40:54 +0900

    Fix : 잘못된 삼항연산자 수정

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,7 +8,8 @@ exports.onRenderBody = ({ setPreBodyComponents }) => {
       dangerouslySetInnerHTML: {
         __html: `
           (() => {    
-            window.__onThemeChange = function() {};                
+            window.__onThemeChange = function() {};      
+
             function setTheme(newTheme) {                  
               window.__theme = newTheme;                  
               preferredTheme = newTheme;                  
@@ -18,23 +19,26 @@ exports.onRenderBody = ({ setPreBodyComponents }) => {
             }
 
             let preferredTheme
+
             try {
               preferredTheme = localStorage.getItem('theme')
             } catch (err) {}
 
             window.__setPreferredTheme = newTheme => {
               setTheme(newTheme)
+
               try {
                 localStorage.setItem('theme', newTheme)
               } catch (err) {}
             }
 
             let darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
             darkQuery.addEventListener('change', e => {
-              window.__setPreferredTheme(e.matches ? 'light' : 'dark')
+              window.__setPreferredTheme(e.matches ? 'dark' : 'light')
             })
 
-            setTheme(preferredTheme || (darkQuery.matches ? 'light' : 'dark'))
+            setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'))
           })()
         `,
       },
