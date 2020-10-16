eb35187ddb5953dee2a3d0a3f2cff00d4a6658ae
최성익 <sungik.dev@gmail.com>
2020-10-16 21:00:46 +0900

    Feat : 소개 페이지 SEO 추가

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SEO from 'components/seo';
 import Layout from 'components/layout/layout';
 
 const About = () => {
   return (
     <Layout>
+      <SEO title="About" />
       <div>소개</div>
     </Layout>
   );
