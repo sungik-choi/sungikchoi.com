eff4ce99b56acb752129805871df03bddecabda0
최성익 <sungik.dev@gmail.com>
2020-10-16 18:31:54 +0900

    Feat : 테마 전환 : ThemeProvider -> css var()
    - 서버사이드렌더링에서 테마 깜빡임 문제가 발생했다.
    - https://blog.rhostem.com/posts/2020-06-25-dark-mode-for-gatsby-website 여길 참고해서 해결책을 찾았음
    - https://dev.to/amaltapalov/how-i-set-dark-mode-for-gatsby-website-4ni0 여기도
    - DOM 트리가 완성되기 전에, 스타일시트를 미리 불러와야 깜빡임 현상을 막을 수 있다.
    - css-in-js 라이브러리로 작성한 스타일은 앱이 구동한 후 추가되기 때문에 css var() 로 바꿔야만 해결할 수 있다

---
+++ gatsby-ssr.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+
+const React = require('react');
+
+exports.onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
+    React.createElement('script', {
+      dangerouslySetInnerHTML: {
+        __html: `
+          (() => {    
+            window.__onThemeChange = function() {};                
+            function setTheme(newTheme) {                  
+              window.__theme = newTheme;                  
+              preferredTheme = newTheme;                  
+              document.body.className = newTheme;
+              document.body.dataset.theme = newTheme;                 
+              window.__onThemeChange(newTheme);                
+            }
+
+            let preferredTheme
+            try {
+              preferredTheme = localStorage.getItem('theme')
+            } catch (err) {}
+
+            window.__setPreferredTheme = newTheme => {
+              setTheme(newTheme)
+              try {
+                localStorage.setItem('theme', newTheme)
+              } catch (err) {}
+            }
+
+            let darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+            darkQuery.addEventListener('change', e => {
+              window.__setPreferredTheme(e.matches ? 'light' : 'dark')
+            })
+
+            setTheme(preferredTheme || (darkQuery.matches ? 'light' : 'dark'))
+          })()
+        `,
+      },
+    }),
+  ]);
+};
