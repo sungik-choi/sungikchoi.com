6a214d1f5275f757abaec3a291068346993af996
최성익 <sungik.dev@gmail.com>
2020-10-20 03:11:34 +0900

    Feat : 그리드 파일 분리 & 인피니티 스크롤 추가
    - 게시글이 많은 경우를 고려해 인피니티 스크롤을 추가
    - 더 보기 버튼 & 히스토리 추가로 변경할 거 같음. 인피니티 스크롤은 UX에 좋지가 않다.. 푸터도 볼 수가 없고. 우선은 구현이 간단하므로 이방법으로 구현

---
+++ src/components/postGrid.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+import Card from 'components/card';
+import { ThumbnailWrapper } from 'components/centeredImg';
+import convertToKorDate from 'utils/convertToKorDate';
+
+const MAX_POST_NUM = 2;
+
+const PostGrid = ({ posts }) => {
+  const [hasMore, setHasMore] = useState(false);
+  const [currentList, setCurrentList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const scrollEdgeRef = useRef(null);
+
+  useEffect(() => {
+    if (posts.length === 0 || isLoading) return;
+    setHasMore(posts.length > MAX_POST_NUM);
+    setCurrentList([...posts.slice(0, MAX_POST_NUM)]);
+    setIsLoading(true);
+  }, [isLoading, posts]);
+
+  useEffect(() => {
+    const loadEdges = () => {
+      const currentLength = currentList.length;
+      const more = currentLength < posts.length;
+      const nextEdges = more
+        ? posts.slice(currentLength, currentLength + MAX_POST_NUM)
+        : [];
+      console.log(currentLength, currentList, nextEdges);
+      setHasMore(more);
+      setCurrentList([...currentList, ...nextEdges]);
+    };
+
+    let observer = null;
+    if (!scrollEdgeRef) return;
+
+    const scrollEdgeElem = scrollEdgeRef.current;
+
+    const option = {
+      rootMargin: '0px 0px 400px 0px',
+      threshold: [0.5],
+    };
+
+    observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasMore) loadEdges();
+    }, option);
+
+    observer.observe(scrollEdgeElem);
+    return () => observer && observer.disconnect();
+  }, [posts, hasMore, currentList]);
+
+  return (
+    <Grid role="list">
+      {currentList.map((data) => {
+        const { id, slug, title, desc, date, category, thumbnail, alt } = data;
+        const korDate = convertToKorDate(date);
+        const ariaLabel = `${title} - ${category} - Posted on ${korDate}`;
+        return (
+          <List key={id} role="listitem">
+            <Link to={slug} aria-label={ariaLabel}>
+              <Card
+                thumbnail={thumbnail}
+                alt={alt}
+                category={category}
+                title={title}
+                desc={desc}
+                date={date}
+                korDate={korDate}
+              />
+            </Link>
+          </List>
+        );
+      })}
+      <div ref={scrollEdgeRef} />
+    </Grid>
+  );
+};
+
+const Grid = styled.ul`
+  display: grid;
+  grid-gap: var(--grid-gap-xl);
+  grid-template-columns: repeat(2, 1fr);
+  list-style: none;
+
+  & > li {
+    margin-bottom: 0;
+  }
+
+  @media (max-width: ${({ theme }) => theme.device.sm}) {
+    grid-gap: var(--grid-gap-lg);
+  }
+`;
+
+const List = styled.li`
+  box-sizing: border-box;
+  grid-column: span 1;
+
+  a {
+    display: block;
+    height: 100%;
+  }
+
+  a:hover ${ThumbnailWrapper}::after, a:focus ${ThumbnailWrapper}::after {
+    opacity: 1;
+  }
+
+  & .gatsby-image-wrapper {
+    transition: opacity 1s ease-out, transform 0.5s ease;
+  }
+
+  a:hover,
+  a:focus {
+    .gatsby-image-wrapper {
+      transform: scale(1.03);
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.device.sm}) {
+    grid-column: span 2;
+  }
+`;
+
+export default PostGrid;
