ca3243bc64a01393ba90e8d3513c9cd205197cae
최성익 <sungik.dev@gmail.com>
2020-10-20 04:57:11 +0900

    Feat : gatsby-meta-config 파일 생성
    - 메타데이터 관련 변수들 이동

---
+++ gatsby-meta-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Sungik Choi',
+  description: `Sungik Choi's Devlog`,
+  author: 'Sungik Choi',
+  utterances: 'sungik-choi/blog-comment',
+  icon: 'src/images/icon.svg',
+  links: {
+    github: 'https://www.github.com/sungik-choi',
+  },
+};
