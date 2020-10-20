c22f790e76469a3f12536712757aac19d77ef473
최성익 <sungik.dev@gmail.com>
2020-10-20 22:29:16 +0900

    Fix : 모바일 관성 스크롤 메뉴 딸려가는 문제 해결

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -139,9 +139,7 @@ const LinkUl = styled.ul`
       margin-left: 0;
       font-size: var(--text-md);
       transform: ${({ toggle }) =>
-        toggle
-          ? `translateY(calc(var(--nav-height) + var(--sizing-lg)))`
-          : `translateY(var(--nav-height))`};
+        toggle ? `translateY(var(--sizing-lg))` : `translateY(0)`};
       opacity: ${({ toggle }) => (toggle ? '1' : '0')};
     }
 
