b3203f6cfdf03e0e4a32ffb29b5e6b73f9b5bfaf
Sungik Choi <sungikchoi@ip-172-30-1-92.ap-northeast-2.compute.internal>
2023-12-10 17:18:18 +0900

    feat: init astro

--- src/utils/convertToKorDate.js
+++
@@ -1,9 +0,0 @@
-const convertToKorDate = (time) => {
-  const date = new Date(time);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  return `${year}년 ${month}월 ${day}일`;
-};
-
-export default convertToKorDate;
